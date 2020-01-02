ccafe4208485b9eb892583601f13a84ef6713407
Grzegorz Gajoch <gajoch@gmail.com>
2020-01-02 00:00:47 +0000

    mocking and testing

--- src-rust/src/lib.rs
+++ src-rust/src/lib.rs
@@ -1,8 +1,7 @@
-#![crate_type = "staticlib"]
-#![feature(lang_items)]
-#![no_std]
-#![no_builtins]
+#![cfg_attr(not(test), no_std)]
+#![feature(proc_macro_hygiene)]
 
+#[cfg(not(test))]
 use panic_semihosting as _;
 
 extern "C" {
@@ -11,22 +10,83 @@ extern "C" {
     fn cxx_delay(ms: u32);
 }
 
-fn led_on() {
-    unsafe { cxx_led_on(); }
+mod wrapper {
+    cfg_if::cfg_if! {
+        if #[cfg(test)] {
+            use mockall::automock;
+
+        }
+    }
+
+    #[cfg_attr(test, automock())]
+    pub(super) mod hal {
+
+        pub fn led_on() {
+            unsafe { crate::cxx_led_on(); }
+        }
+
+        pub fn led_off() {
+            unsafe { crate::cxx_led_off(); }
+        }
+
+        pub fn delay(ms: u32) {
+            unsafe { crate::cxx_delay(ms); }
+        }
+    }
 }
-fn led_off() {
-    unsafe { cxx_led_off(); }
+
+cfg_if::cfg_if! {
+    if #[cfg(test)] {
+        use wrapper::mock_hal as hal;
+    } else {
+        use wrapper::hal;
+    }
 }
-fn delay(ms: u32) {
-    unsafe { cxx_delay(ms); }
+
+fn loop_iteration() {
+    hal::led_on();
+    hal::delay(100);
+    hal::led_off();
+    hal::delay(200);
 }
 
 #[no_mangle]
 pub extern "C" fn rust_main() {
     loop {
-        led_on(); 
-        delay(100);
-        led_off();
-        delay(200);
+        loop_iteration();
+    }
+}
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use mockall::*;
+    use mockall::predicate::*;
+    use crate::loop_iteration;
+    use crate::hal;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+
+    #[test]
+    fn loop_turns_on() {
+        let mut seq = Sequence::new();
+
+        let led_on_ctx = hal::led_on_context();
+        let led_off_ctx = hal::led_off_context();
+        let delay_ctx = hal::delay_context();
+
+        led_on_ctx.expect().times(1).returning(|| ()).in_sequence(&mut seq);
+        delay_ctx.expect().with(eq(100)).times(1).in_sequence(&mut seq);
+        led_off_ctx.expect().times(1).in_sequence(&mut seq);
+        delay_ctx.expect().with(eq(200)).times(1).in_sequence(&mut seq);
+
+        loop_iteration();
     }
 }
